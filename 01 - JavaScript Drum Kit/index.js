cb6399ed599ad61a0aa3b2c2d16dab91ca43e374
volha <v.khviadchenia@gmail.com>
2020-12-28 23:51:38 +0100

    feat: add EventListener for window

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,1 +1,11 @@
-// alert('Hey-hop')
+window.addEventListener('keydown', (e) => {
+  const key = document.querySelector(`.key[data-key = '${e.keyCode}']`);
+  const audio = document.querySelector(`audio[data-key = '${e.keyCode}']`);
+  key.classList.add('playing');
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+})
+
+const keys = document.querySelectorAll('.key');
+keys.forEach((el) => el.addEventListener('transitionend', removeTransition))
