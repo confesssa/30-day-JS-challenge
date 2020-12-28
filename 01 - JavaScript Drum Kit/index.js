66ec524d72700c14ac93f1a64973ff732f9a8c2e
volha <v.khviadchenia@gmail.com>
2020-12-29 00:15:04 +0100

    feat: add function playSound

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,11 +1,19 @@
-window.addEventListener('keydown', (e) => {
+
+function playSound(e) {
   const key = document.querySelector(`.key[data-key = '${e.keyCode}']`);
   const audio = document.querySelector(`audio[data-key = '${e.keyCode}']`);
   key.classList.add('playing');
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
-})
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
 
 const keys = document.querySelectorAll('.key');
 keys.forEach((el) => el.addEventListener('transitionend', removeTransition))
+
+window.addEventListener('keydown', playSound);
