c364f38cf80a9b22169968ca9c10f128b8363de0
volha <v.khviadchenia@gmail.com>
2020-12-29 01:13:16 +0100

    feat: add function for mouseEvents

--- 01 - JavaScript Drum Kit/index.js
+++ 01 - JavaScript Drum Kit/index.js
@@ -1,5 +1,5 @@
 
-function playSound(e) {
+function playByKeyboard(e) {
   const key = document.querySelector(`.key[data-key = '${e.keyCode}']`);
   const audio = document.querySelector(`audio[data-key = '${e.keyCode}']`);
   key.classList.add('playing');
@@ -13,7 +13,17 @@ function removeTransition(e) {
   this.classList.remove('playing');
 }
 
+function playByMouse(e) {
+  const keyCode = this.getAttribute('data-key');
+	const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+	audio.currentTime = 0;
+	audio.play();
+	const key = document.querySelector(`.key[data-key="${keyCode}"]`);
+  key.classList.add('playing');
+}
+
 const keys = document.querySelectorAll('.key');
 keys.forEach((el) => el.addEventListener('transitionend', removeTransition))
+keys.forEach((el) => el.addEventListener('click', playByMouse));
 
-window.addEventListener('keydown', playSound);
+window.addEventListener('keydown', playByKeyboard);
