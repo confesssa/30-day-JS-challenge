7fa4316baac0b65b96d146ffac758f1a107ce1cc
volha <v.khviadchenia@gmail.com>
2021-01-23 03:14:20 +0100

    feat: add function for minutes

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -1,11 +1,19 @@
 secondHand = document.querySelector('.second-hand');
+minuteHand = document.querySelector('.min-hand');
 
 function setDate() {
   const now = new Date;
+
   const sec = now.getSeconds();
-  const secDegree = ((sec / 60) * 360);
+  const secDegree = ((sec / 60) * 360 + 90);
   secondHand.style.transform = `rotate(${secDegree}deg)`;
+
+  const min = now.getMinutes();
+  const minDegree = ((min / 60) * 360 + 90);
+  minuteHand.style.transform = `rotate(${minDegree}deg)`;
+
   
 };
 
+setInterval(setDate, 1000);
 setInterval(setDate, 1000);
